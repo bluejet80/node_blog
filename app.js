d6bd1c1697d215374a339a33b6fbcb3ea06f937e
bluejet80 <know_jarett@yahoo.com>
2023-05-15 10:14:45 -0500

    ready to start css

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
+//require routes
+const mainRoute = require("./server/routes/main");
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -13,6 +16,16 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
+// static files
+
+app.use(express.static("public"));
+
 // mount the route
 
-app.use("/", require("./server/routes/main"));
+app.use("/", mainRoute);
+
+// run server
+
+app.listen(PORT, () => {
+  console.log(`Server is Listening on port ${PORT}`);
+});
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,3 +1,21 @@
 const express = require("express");
 
-const router = express();
+const router = express.Router();
+
+const locals = {
+  title: "NodeJS Blog",
+  description: "Blog mand with Nodejs and Express and EJS and MongoDB.",
+  author: "Jarett Young",
+};
+
+// Routes
+router.get("", (req, res) => {
+  res.render("index", locals);
+});
+
+// Routes
+router.get("/about", (req, res) => {
+  res.render("about", locals);
+});
+
+module.exports = router;
